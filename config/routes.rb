485c2e4f34f3c6d0ca69846e95a11ffeddcb2cdf
leeelyse <elyselee2016@kellogg.northwestern.edu>
2015-05-11 14:08:35 -0500

    week6 homework

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
+get("/photos/new", { :controller => "photos", :action => "new_form" })
+get("/create_photo", { :controller => "photos", :action => "create_row" })
+get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
+
 end
